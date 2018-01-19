dabc1749edeb03e65a4745d9d92bbdfc63116e4a
lichddd <lichddd@126.com>
2018-01-19 09:40:07 +0800

    可自定义图片

--- static/config.js
+++ static/config.js
@@ -4,9 +4,9 @@ window.config=window.config||{};
 
 
 window.config.imgs=window.config.imgs||[
-`static/img/1.jpeg`,
-`static/img/2.jpeg`,
-`static/img/3.jpeg`,
-`static/img/4.jpeg`,
+'static/img/1.jpeg',
+'static/img/2.jpeg',
+'static/img/3.jpeg',
+'static/img/4.jpeg',
 
 ];
